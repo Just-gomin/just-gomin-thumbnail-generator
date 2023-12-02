a0eb652cc2e6dfa215fe9e08113a46e9c61201d1
안강민 <hkkm612@gmail.com>
2023-12-02 22:48:51 +0900

    create. [APP] Commander 패키지 설치 및 옵션 정의

---
+++ bin/index.js
@@ -0,0 +1,36 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const commander_1 = require("commander");
+const log = console.log;
+const bootstrap = () => {
+    const program = new commander_1.Command();
+    program
+        .version("0.1.0")
+        .description("Just-gomin's Blog Thumbnail Generator")
+        .option("-c, --category <category>", "Set the category of Content.")
+        .option("-t, --title <title>", "Set the title of Content.")
+        .option("-s, --size <size>", "Set the size of Thumbnail Image's side length.(Unit is 'px')");
+    const options = program.opts();
+    let category = "";
+    let title = "";
+    let isErr = false;
+    let errMsg = "You must enter the ";
+    log(process.argv.length);
+    if (!options.category || !options.title) {
+        isErr = true;
+        const categoryKey = "'Category'";
+        const titleKey = "'Title'";
+        errMsg +=
+            !options.category && !options.title
+                ? `${categoryKey} & ${titleKey}`
+                : !options.category
+                    ? categoryKey
+                    : titleKey;
+        errMsg += " of your content.";
+    }
+    if (isErr)
+        log(errMsg);
+    else {
+    }
+};
+bootstrap();
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,48 @@
-function main() {
-  console.log("Just-gomin Thumbnail Generator");
-}
+import { Command } from "commander";
 
-main();
+const log = console.log;
+
+const bootstrap = () => {
+  const program = new Command();
+
+  program
+    .version("0.1.0")
+    .description("Just-gomin's Blog Thumbnail Generator")
+    .option("-c, --category <category>", "Set the category of Content.")
+    .option("-t, --title <title>", "Set the title of Content.")
+    .option(
+      "-s, --size <size>",
+      "Set the size of Thumbnail Image's side length.(Unit is 'px')"
+    );
+
+  const options = program.opts();
+
+  let category: string = "";
+  let title: string = "";
+
+  let isErr: boolean = false;
+  let errMsg: string = "You must enter the ";
+
+  log(process.argv.length);
+
+  if (!options.category || !options.title) {
+    isErr = true;
+    const categoryKey = "'Category'";
+    const titleKey = "'Title'";
+
+    errMsg +=
+      !options.category && !options.title
+        ? `${categoryKey} & ${titleKey}`
+        : !options.category
+        ? categoryKey
+        : titleKey;
+
+    errMsg += " of your content.";
+  }
+
+  if (isErr) log(errMsg);
+  else {
+  }
+};
+
+bootstrap();
